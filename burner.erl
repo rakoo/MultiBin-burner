72a5586a890202839023caab59282d27155f0523
rakoo <matthieu.rakotojaona@gmail.com>
2012-06-03 14:16:18 +0200

    Add credentials management (basic auth only)

--- burner.erl
+++ burner.erl
@@ -1,5 +1,5 @@
 -module(burner).
--export([start/0, iso8601date/0]).
+-export([start/0]).
 
 start() ->
   application:start(sasl),
@@ -8,7 +8,10 @@ start() ->
   application:start(public_key),
   application:start(ssl),
   application:start(couchbeam),
-  Server = couchbeam:server_connection("localhost", 5984, "", []),
+  {ok, Cfg} = file:consult("credentials"),
+  {Username, Password} = extract_credentials(Cfg),
+  Server = couchbeam:server_connection("localhost", 5984, "",
+    [{basic_auth, {Username, Password}}]),
   {ok, Db} = couchbeam:open_db(Server, "multibin-test", []),
   {ok, ViewResults} = couchbeam_view:fetch(Db, {"MultiBin",
       "expire_date"}, [{start_key, iso8601date()}, descending,
@@ -17,7 +20,7 @@ start() ->
           couchbeam_doc:get_rev(X)}]} 
     || {[_,_,_, {<<"doc">>,X}]} <- ViewResults],
   {ok, Result} = couchbeam:delete_docs(Db, RelevantDocs),
-  io:format("~s~n", [Result]).
+  io:format("~p~n", [Result]).
   
 iso8601date() ->
   {{Y,Mo,D}, {H,Mn,S}} = erlang:localtime(),
@@ -25,3 +28,17 @@ iso8601date() ->
   FmtStr = "~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0B.~3.10.0BZ",
   IsoStr = io_lib:format(FmtStr, [Y, Mo, D, H, Mn, S, Micro div 1000]),
   list_to_binary(IsoStr).
+
+extract_credentials(Cfg) ->
+  Username = proplist_get_value(username, Cfg),
+  Password = proplist_get_value(password, Cfg),
+  {Username, Password}.
+
+proplist_get_value(Key,List)->
+  case lists:keyfind(Key,1,List) of
+    {_K,V}->
+      V;
+    false->
+      io:format("key ~p wasn't found in list ~p~n", [Key, List]),
+      undefined
+  end.
